3bc28aebc14f91264bd30734dd8f93cadc6fab7e
Joe Reed <joe@kiteframe.co.uk>
2020-12-24 14:16:37 +0000

    Day 23 - puzzle 2

--- day-23/day-23.go
+++ day-23/day-23.go
@@ -3,66 +3,95 @@ package main
 import "container/ring"
 
 func puzzle1(input []int, moves int) int {
-	cups := ring.New(len(input))
-	for _, cup := range input {
-		cups.Value = cup
-		cups = cups.Next()
-	}
+	cups, cupsMap := parseInput(input)
+	len := len(input)
 	for i := 0; i < moves; i++ {
-		cups = performMove(cups)
-	}
-	return getAnswer(cups)
-}
+		pickedUp := cups.Unlink(3)
 
-func performMove(cups *ring.Ring) *ring.Ring {
-	current := cups.Value.(int)
-	pickedUp := cups.Unlink(3)
-	destination := current - 1
-	for cups.Value != destination {
-		cups = cups.Next()
-		if cups.Value == current {
+		destination := cups.Value.(int) - 1
+		if destination == 0 {
+			destination = len
+		}
+		for contains(pickedUp, destination) {
 			destination--
+			if destination == 0 {
+				destination = len
+			}
 		}
-		if destination < min(cups) {
-			destination = max(cups)
-		}
-	}
-	cups.Link(pickedUp)
-	for cups.Value != current {
+
+		cupsMap[destination].Link(pickedUp)
+
 		cups = cups.Next()
 	}
 
-	return cups.Next()
-}
-
-func getAnswer(cups *ring.Ring) (result int) {
-	for cups.Value != 1 {
-		cups = cups.Next()
-	}
-	cups.Next()
+	cups = cupsMap[1].Next()
+	result := 0
 	cups.Do(func(cup interface{}) {
 		if cup.(int) != 1 {
 			result = result*10 + cup.(int)
 		}
 	})
+	return result
+}
+
+func puzzle2(input []int) int {
+	cups, cupsMap := parseInput(input)
+	remainingCups, cupsMap := getRemainingCups(input, cupsMap)
+	cups.Prev().Link(remainingCups)
+
+	len := cups.Len()
+	for i := 0; i < 10000000; i++ {
+		pickedUp := cups.Unlink(3)
+
+		destination := cups.Value.(int) - 1
+		if destination == 0 {
+			destination = len
+		}
+		for contains(pickedUp, destination) {
+			destination--
+			if destination == 0 {
+				destination = len
+			}
+		}
+
+		cupsMap[destination].Link(pickedUp)
+
+		cups = cups.Next()
+	}
+
+	cup1 := cupsMap[1]
+	return cup1.Move(1).Value.(int) * cup1.Move(2).Value.(int)
+}
+
+func parseInput(input []int) (cups *ring.Ring, cupsMap map[int]*ring.Ring) {
+	cups = ring.New(len(input))
+	cupsMap = make(map[int]*ring.Ring)
+	for _, cup := range input {
+		cups.Value = cup
+		cupsMap[cup] = cups
+		cups = cups.Next()
+	}
 	return
 }
 
-func min(cups *ring.Ring) int {
-	var min = cups.Value.(int)
-	cups.Do(func(cup interface{}) {
-		if cup.(int) < min {
-			min = cup.(int)
-		}
-	})
-	return min
+func getRemainingCups(input []int, cupsMap map[int]*ring.Ring) (*ring.Ring, map[int]*ring.Ring) {
+	size := 1000000
+	cups := ring.New(size - len(input))
+	for i := len(input) + 1; i <= size; i++ {
+		cups.Value = i
+		cupsMap[i] = cups
+		cups = cups.Next()
+	}
+	return cups, cupsMap
 }
 
-func max(cups *ring.Ring) (max int) {
+func contains(cups *ring.Ring, c int) bool {
+	result := false
 	cups.Do(func(cup interface{}) {
-		if cup.(int) > max {
-			max = cup.(int)
+		if cup.(int) == c {
+			result = true
+			return
 		}
 	})
-	return
+	return result
 }
--- day-23/day-23_test.go
+++ day-23/day-23_test.go
@@ -30,12 +30,35 @@ func TestPuzzle1(t *testing.T) {
 		}
 	}
 }
+func BenchmarkPuzzle1(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		puzzle1([]int{3, 6, 4, 2, 8, 9, 7, 1, 5}, 100)
+	}
+}
 
 func TestSolvePuzzle1(t *testing.T) {
-	fmt.Println("Puzzle 1:",
-		puzzle1(
-			[]int{3, 6, 4, 2, 8, 9, 7, 1, 5},
-			100,
-		),
-	)
+	fmt.Println("Puzzle 1:", puzzle1([]int{3, 6, 4, 2, 8, 9, 7, 1, 5}, 100))
+}
+
+func TestPuzzle2(t *testing.T) {
+	tests := []struct {
+		input    []int
+		expected int
+	}{
+		{
+			[]int{3, 8, 9, 1, 2, 5, 4, 6, 7},
+			149245887792,
+		},
+	}
+
+	for _, test := range tests {
+		a := puzzle2(test.input)
+		if a != test.expected {
+			t.Errorf("expected: %d, actual: %d", test.expected, a)
+		}
+	}
+}
+
+func TestSolvePuzzle2(t *testing.T) {
+	fmt.Println("Puzzle 2:", puzzle2([]int{3, 6, 4, 2, 8, 9, 7, 1, 5}))
 }
